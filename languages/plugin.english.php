dce815e0d559add9b93c2274b9b4af3cb44f5e47
Shitiz Garg <mail@dragooon.net>
2012-05-29 01:20:58 +0530

    Add basic template and language file with sidebar block

---
+++ languages/plugin.english.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Language file for the modification
+ * 
+ * @package Dragooon:WeNotif
+ * @author Shitiz "Dragooon" Garg <Email mail@dragooon.net> <Url http://smf-media.com>
+ * @copyright 2012, Shitiz "Dragooon" Garg <mail@dragooon.net>
+ * @license
+ *		Licensed under "New BSD License (3-clause version)"
+ *		http://www.opensource.org/licenses/BSD-3-Clause
+ * @version 1.0
+ */
+
+$txt['notification_unread_title'] = 'Unread Notifications';
+?>
---
+++ templates/plugin.template.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Template file for notifications
+ * 
+ * @package Dragooon:WeNotif
+ * @author Shitiz "Dragooon" Garg <Email mail@dragooon.net> <Url http://smf-media.com>
+ * @copyright 2012, Shitiz "Dragooon" Garg <mail@dragooon.net>
+ * @license
+ *		Licensed under "New BSD License (3-clause version)"
+ *		http://www.opensource.org/licenses/BSD-3-Clause
+ * @version 1.0
+ */
+
+function template_notifications_block()
+{
+	global $txt, $context, $settings, $scripturl;
+
+	echo '
+	<section>
+		<span class="note', $context['unread_notifications'] ? 'nice' : '', '" style="font-size: 9pt;">', $context['unread_notifications'], '</span>&nbsp;
+		<a href="', $scripturl, '?action=notifications" class="title">', $txt['notification_unread_title'], '</a>';
+	foreach ($context['quick_notifications'] as $notification)
+	{
+		echo '
+			<p class="description" onclick="document.location = \'', $scripturl, '?action=notification;area=redirect;id=', $notification->getID(), '\'">
+				', $notification->getText(), '
+			</p>';
+	}
+	echo '
+	</section>';
+}
+?>
