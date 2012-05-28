d96af2715a661be66b87c5713eec3af032088e75
Shitiz Garg <mail@dragooon.net>
2012-05-28 21:28:19 +0530

    Add the main plugin.php file with the Notifier's interface

---
+++ plugin.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * WeNotif's main plugin file
+ * 
+ * @package Dragooon:WeNotif
+ * @author Shitiz "Dragooon" Garg <Email mail@dragooon.net> <Url http://smf-media.com>
+ * @copyright 2012, Shitiz "Dragooon" Garg <mail@dragooon.net>
+ * @license
+ *		Licensed under "New BSD License (3-clause version)"
+ *		http://www.opensource.org/licenses/BSD-3-Clause
+ * @version 1.0
+ */
+
+if (!defined('WEDGE'))
+	die('File cannot be requested directly');
+
+/**
+ * Notifier interface, every notifier adding their own stuff must implement this interface
+ */
+interface Notifier
+{
+	/**
+	 * Callback for getting the URL of the object
+	 *
+	 * @access public
+	 * @param Notification $notification
+	 * @return string A fully qualified HTTP URL
+	 */
+	public function getURL(Notification $notification);
+
+	/**
+	 * Callback for getting the text to display on the notification screen
+	 *
+	 * @access public
+	 * @param Notification $notification
+	 * @return string The text this notification wants to display
+	 */
+	public function getText(Notification $notification);
+
+	/**
+	 * Returns the name of this notifier
+	 *
+	 * @access public
+	 * @return string
+	 */
+	public function getName();
+}
+?>
