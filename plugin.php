1891b18b951bc0610d876d609179656f5b366c8b
Shitiz Garg <mail@dragooon.net>
2013-03-08 14:50:03 +0530

    Update copyright year notices

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@
  * 
  * @package Dragooon:WeNotif
  * @author Shitiz "Dragooon" Garg <Email mail@dragooon.net> <Url http://smf-media.com>
- * @copyright 2012, Shitiz "Dragooon" Garg <mail@dragooon.net>
+ * @copyright 2012-2013, Shitiz "Dragooon" Garg <mail@dragooon.net>
  * @license
  *		Licensed under "New BSD License (3-clause version)"
  *		http://www.opensource.org/licenses/BSD-3-Clause
